9dbd5c346bc5f555f2e8299df984c64edd086be8
bordelais <vbordelais@affinisoft.com>
2019-10-30 10:54:49 +0100

    Login et reception server.js

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
@@ -17,7 +16,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Validators, FormBuilder, FormGroup } from '@angular/forms';
-// import { freemem } from 'os';
+import {FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Injectable()
 export class DataService {
@@ -15,24 +14,58 @@ export class DataService {
   styleUrls: ['./login.page.scss'],
 })
 
-export class LoginPage implements OnInit {
-  constructor(public http: HttpClient) { }
-  ngOnInit() { }
-  email: any;
-  password: any;
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
+  errorMessages = {
+    email: [
+      {type: 'required', message: 'Le champs Email est requis.'},
+      {type: 'minlength', message: 'Votre email doit contenir au moins 6 caractères.'},
+      {type: 'maxlength', message: 'Votre email ne doit contenir plus de 50 caractères.'},
+      {type: 'pattern', message: 'Merci de saisir un email valide.' }
+    ],
+    password: [
+      {type: 'required', message: 'Le champs Mot de passe est requis.'},
+      {type: 'minlength', message: 'Votre mot de passe doit contenir au moins 6 caractères.'},
+      {type: 'maxlength', message: 'Votre mot de passe ne doit contenir plus de 30 caractères.'},
+      {type: 'pattern', message: 'Votre mot de passe doit contenir au moins 1 nombre, 1 minuscule et 1 majuscule.'}
+    ]
+  };
 
+  constructor(
+    public formBuilder: FormBuilder,
+    public http: HttpClient
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(50),
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(50),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ]))
+    });
+  }
 
-  loginButton(email: any, password: any) {
-    email = this.email;
-    password = this.password;
-    const user = [email, password];
-    // console.log(user);
+  ngOnInit() { }
 
+  loginButton(email: any, password: any) {
+    console.log('email: ', this.loginForm.value.email);
+    console.log('password: ', this.loginForm.value.password);
+    email = this.loginForm.value.email;
+    password = this.loginForm.value.password;
+    const user = [email, password];
+    console.log(user);
 
-
-    this.http.post('http://localhost:8080/login', user).subscribe(data => {
+    this.http.post('http://localhost:8080/login', user).subscribe(data => {
       console.log(data);
-    });
+    });
+
   }
 }
+
